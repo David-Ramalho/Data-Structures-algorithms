cd811382fbec95af914c4f46a16dbf217aa6df5e
David-Ramalho <86934295+David-Ramalho@users.noreply.github.com>
2022-08-23 17:59:44 -0300

    Work and work

--- Estrutura de dados 2022/Grafos/Conceito/main.c
+++ Estrutura de dados 2022/Grafos/Conceito/main.c
@@ -9,6 +9,8 @@ int op=1;
 
 //Prototipação:
 int grafo_tam();
+void grafo_insert();
+void grafo_remove();
 
  
 
@@ -36,4 +38,40 @@ int grafo_tam(){
 	printf("Escolha a quantidade de vertices do grafo: ");
 	scanf("%d", &tam);
 	return tam;
+}
+
+void grafo_insert(){
+	int num1, num2;
+	system("cls");
+	printf("Escolha o vertice de origem entre 0 a %d:", tam-1);
+	scanf("%d", &num1);
+	printf("Escolha o vertice de destino entre 0 a %d:", tam-1);
+	scanf("%d", &num2);
+	
+	if(num1>tam-1 || num2>tam-1|| num1<0 || num2<0){
+		printf("\nos valors precisam estar entre 0 e %d\n\n", tam);
+		system("pause");
+	}
+	else{
+		mat[num1][num2]=1;
+		mat[num2][num1]=1;
+	}
+}
+
+void grafo_remover(){
+		int num1, num2;
+	system("cls");
+	printf("Escolha o vertice de origem entre 0 a %d:", tam-1);
+	scanf("%d", &num1);
+	printf("Escolha o vertice de destino entre 0 a %d:", tam-1);
+	scanf("%d", &num2);
+	
+	if(num1>tam-1 || num2>tam-1|| num1<0 || num2<0){
+		printf("\nos valors precisam estar entre 0 e %d\n\n", tam);
+		system("pause");
+	}
+	else{
+		mat[num1][num2]=0;
+		mat[num2][num1]=0;
+	}
 }
