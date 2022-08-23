5cb463e8f9326853f877ab28ec22e0e4aaa6ce76
David-Ramalho <86934295+David-Ramalho@users.noreply.github.com>
2022-08-23 20:51:39 -0300

    Deep search Graph algorithm
    Work in progress.

---
+++ Estrutura de dados 2022/Grafos/Deep Search Graph/main.c
@@ -0,0 +1,22 @@
+//Bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+//Número máximo de vértices
+#define MAXV 8
+
+//Estrutura de um nó
+typedef struct str_no {
+int id;
+struct str_no *proximo;
+} str_no;
+
+//Grafo
+struct str_no grafo[MAXV];
+
+
+
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
